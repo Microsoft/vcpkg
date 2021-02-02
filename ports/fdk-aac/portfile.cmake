3a734619b1f1fabaefc62f0a7374ed065d9b4812
Be <be@mixxx.org>
2021-02-02 15:51:15 -0600

    [fdk-aac] make patent-encumbered HE-AAC optional; update version
    By default, use a fork of fdk-aac (which was originally written
    by Fraunhofer for the Android Open Source Project) which has
    support for the patent-encumbered HE-AAC, HE-AACv2, and xHE-AAC
    profiles removed. This fork is used by Fedora and Arch Linux to
    provide support for most AAC use cases without patent licensing
    fees and to permit combining it with GPL software. The upstream
    fdk-aac with support for all AAC profiles can be built with the
    new 'he-aac' option for this port.
    
    Fedora Legal's opinion, from
    https://bugzilla.redhat.com/show_bug.cgi?id=1501522#c112 :
    The Fedora Project is aware that the Free Software Foundation
    has stated that the Fraunhofer FDK AAC license is GPL
    incompatible, specifically, because of Clause 3.
    
    We believe that the fdk-aac software codec implementation that we
    wish to include in Fedora is no longer encumbered by AAC patents.
    This fact means that Clause 3 in the FDK AAC license is a "no op",
    or to put it plainly, if no patents are in play, there are no
    patent licenses to disclaim. For this (and only this) specific
    implementation of fdk-aac, we believe that the FDK AAC license is
    GPL compatible.

--- ports/fdk-aac/portfile.cmake
+++ ports/fdk-aac/portfile.cmake
@@ -1,15 +1,22 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
-vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO mstorsjo/fdk-aac
-  REF e45ae429b9ca8f234eb861338a75b2d89cde206a
-  SHA512 e4d0ec632a67642312bd0c812849601452d4ba45b31bc2f2a9392bba5fe2320b2099c5c7077c9571ea270804979039182060dc1acacdc397ca2a9b8ca43301a3
-  HEAD_REF master
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    he-aac HE_AAC
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/fdk-aac.def DESTINATION ${SOURCE_PATH})
+if(HE_AAC)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO mstorsjo/fdk-aac
+        REF 77ee4dd31e8812a589e03f03347f592ef8260f4f
+        SHA512 f7ade591dbfe2ee9e383955de13f5d1d122ed7bb3e52dd3b3b3b59af716f5057b099628b290a3b62ccef1539e66bcbb0ec7b7ca2ed0b57d8f30e575d2a1ba8a5
+        HEAD_REF master
+    )
+else()
+    vcpkg_from_git(
+        OUT_SOURCE_PATH SOURCE_PATH
+        URL https://gitlab.freedesktop.org/Be/fdk-aac-stripped.git
+        REF f6e49829e17892045de223fffbc9bd704c3be2be
+    )
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -18,6 +25,10 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/fdk-aac)
-file(INSTALL ${SOURCE_PATH}/NOTICE DESTINATION ${CURRENT_PACKAGES_DIR}/share/fdk-aac RENAME copyright)
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
+
+file(INSTALL ${SOURCE_PATH}/NOTICE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
