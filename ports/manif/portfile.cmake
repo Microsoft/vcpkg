af8a1c7461d030fb41617466f37f4a204ad1a709
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-03-04 17:05:00 +0100

    [manif] Fix style in portfile.cmake

--- ports/manif/portfile.cmake
+++ ports/manif/portfile.cmake
@@ -13,11 +13,9 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
 
 # Add the copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
